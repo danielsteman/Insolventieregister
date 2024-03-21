af0def81edecb4d6700e3e95d551be718f661312
Daniel Steman <daniel-steman@live.nl>
2024-03-21 22:29:00 +0100

    Add soap header

--- Insolventieregister/Program.cs
+++ Insolventieregister/Program.cs
@@ -1,24 +1,47 @@
 ﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        // Instantiate the service client
         var client = new InsolvencyService.CIRSoapClient(InsolvencyService.CIRSoapClient.EndpointConfiguration.CIRSoap);
 
         try
         {
             var date = DateTime.Now;  // Example date, replace with actual date
             var court = "exampleCourt";  // Replace with actual court
-            var pubType = new InsolvencyService.ArrayOfString();  // Initialize with new ArrayOfString
+            var pubType = new InsolvencyService.ArrayOfString
+            {
+                "PublicationType1", // Add example publication types
+                "PublicationType2"
+            };  // Initialize with new ArrayOfString
 
-            // Invoke the service method
-            var response = await client.searchByDateAsync(date, court, pubType);
+            using (new OperationContextScope(client.InnerChannel))
+            {
+                var actionHeader = MessageHeader.CreateHeader("Action", "http://schemas.xmlsoap.org/ws/2004/08/addressing", "http://www.rechtspraak.nl/namespaces/cir01/searchByDate");
+                OperationContext.Current.OutgoingMessageHeaders.Add(actionHeader);
 
-            // Handle the response
-            // Access response data from 'response.Body'
-            Console.WriteLine("Response received: " + response.Body.ToString());
+                // Add custom headers for username and password
+                MessageHeader<string> usernameHeader = new MessageHeader<string>("username");
+                MessageHeader<string> passwordHeader = new MessageHeader<string>("password");
+
+                // Create header untyped
+                var untypedUsernameHeader = usernameHeader.GetUntypedHeader("Username", "");
+                var untypedPasswordHeader = passwordHeader.GetUntypedHeader("Password", "");
+
+                // Add headers to the outgoing request
+                OperationContext.Current.OutgoingMessageHeaders.Add(untypedUsernameHeader);
+                OperationContext.Current.OutgoingMessageHeaders.Add(untypedPasswordHeader);
+
+                // Invoke the service method
+                var response = await client.searchByDateAsync(date, court, pubType);
+
+                // Handle the response
+                // Access response data from 'response.Body'
+                Console.WriteLine("Response received: " + response.Body.ToString());
+            }
         }
         catch (Exception ex)
         {
